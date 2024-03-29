4f190f12e0c6841f48f1010c35db6573a6329ab1
Abhishek Nath Tiwari <110283974+abhint1@users.noreply.github.com>
2024-03-29 09:36:15 +0000

    1

---
+++ src/pages/Info.js
@@ -0,0 +1,29 @@
+import React from 'react'
+const Info = () => {
+  return (
+    <div className='Info'>
+      <div className='aboutSection'>
+        <h1 className='aboutheading'>ABOUT US</h1>
+        <div className='aboutparadiv'>
+        <p className='aboutpara'>BitBlocks offers a ​​diverse range of ​​educational resources ​​and interactive ​​experiences designed ​​to demystify ​​blockchain technology.</p>
+        </div>
+      </div>
+      <div className='blocksection'>
+        <div className='redblock'>
+          <div className='blackblock'>
+            <h2 className='bbheading'>why choose bitblocks?</h2>
+            <div className='bblist'>
+              <li className='lis'>Innovative Approach</li>
+              <li className='lis'>Comprehensive Content</li>
+              <li className='lis'>Accessible Learning</li>
+              <li className='lis'>Fun Games</li>
+            </div>
+            <button className="button-24">Learn</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Info
